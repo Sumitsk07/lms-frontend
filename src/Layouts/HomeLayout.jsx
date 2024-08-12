7a29eeb1c7c7f1888f337fc739f980a3f5206559
Sumitsk07 <sumit.g.kamble2000@gmail.com>
2024-08-12 21:01:12 +0530

    Added logout functionality

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -4,8 +4,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 
 import Footer from "../Components/Footer";
+import { logout } from "../Redux/Slices/AuthSlice";
 function HomeLayout({ children }) {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   // for checking is user is logged in
@@ -27,12 +28,11 @@ function HomeLayout({ children }) {
     drawerSide[0].style.width = "0";
   }
 
-  function handleLogout(e) {
+  async function handleLogout(e) {
     e.preventDefault();
 
-    // const res = await dispatch(logout());
-    // if(res?.payload?.success)
-    navigate("/");
+    const res = await dispatch(logout());
+    if (res?.payload?.success) navigate("/");
   }
 
   return (
